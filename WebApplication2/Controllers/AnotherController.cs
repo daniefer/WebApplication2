fd8cd0d37bfabe741eea4eabd177537f5f3634bd
Peter Slavin <pbslavin@gmail.com>
2018-07-21 10:16:38 -0500

    Add project files.

---
+++ WebApplication2/Controllers/AnotherController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    public class AnotherController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new Models.Greeting();
+            model.CurrentTime = DateTime.Today.AddHours(5);
+            model.FirstName = "Peter";
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication2/Models/Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Greeting
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string HomeCity { get; set; }
+
+        public int PetCount { get; set; }
+
+        public DateTime CurrentTime { get; set; }
+    }
+}
